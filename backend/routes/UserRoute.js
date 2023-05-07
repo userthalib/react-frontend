5fcba3b73a19e7a32828011e5b6b3564c7c8426f
Irfan Thalib A <thalibirfan29@gmail.com>
2023-05-07 22:03:28 +0700

    add auth middleware

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -6,19 +6,14 @@ import {
     updateUser,
     deleteUser
 } from "../controllers/Users.js";
-// import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
+import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
-router.post('/users', createUser);
-router.patch('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
-// router.get('/users', verifyUser, adminOnly, getUsers);
-// router.get('/users/:id', verifyUser, adminOnly, getUserById);
-// router.post('/users', verifyUser, adminOnly, createUser);
-// router.patch('/users/:id', verifyUser, adminOnly, updateUser);
-// router.delete('/users/:id', verifyUser, adminOnly, deleteUser);
+router.get('/users', verifyUser, adminOnly, getUsers);
+router.get('/users/:id', verifyUser, adminOnly, getUserById);
+router.post('/users', verifyUser, adminOnly, createUser);
+router.patch('/users/:id', verifyUser, adminOnly, updateUser);
+router.delete('/users/:id', verifyUser, adminOnly, deleteUser);
 
 export default router;
